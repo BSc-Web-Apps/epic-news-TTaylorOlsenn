9f9f48131e270a764de57b54ccaeacc87e9d93e6
TTaylorOlsenn <0325055@hc.ac.je>
2025-11-20 09:36:19 +0000

    Revert "foundation"
    This reverts commit 933a2b8cad687c5ac809e3a5750156ddd5140213.

--- components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer>
-            <h1>And this is the main</h1>
-        </footer>
-    )
-}
--- components/Main.jsx
+++
@@ -1,7 +0,0 @@
-export default function Main() {
-    return (
-        <main>
-            <h1>This is the main</h1>
-        </main>
-    )
-}
