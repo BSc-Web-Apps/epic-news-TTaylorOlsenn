13389135ee86c770c54a73fdf50f80bcfcef90ca
TTaylorOlsenn <0325055@hc.ac.je>
2025-12-04 11:22:33 +0000

    Small changes
    * Commented out CSRF mitigation due to bug
    * Added spacing between different sections in home page

--- app/root.tsx
+++ app/root.tsx
@@ -1,10 +1,13 @@
 import { Outlet, useLoaderData } from 'react-router'
+import { AuthenticityTokenProvider } from 'remix-utils/csrf/react'
 import { type Route } from './+types/root.ts'
 import { type loader } from './__root.server.tsx'
 import { GeneralErrorBoundary } from './components/error-boundary.tsx'
 import FooterMenuRight from './components/organisms/Footer/FooterMenuRight.tsx'
 import HeaderWithSearch from './components/organisms/HeaderWithSearch/index.tsx'
 import Document from './components/shared-layout/Document.tsx'
+import { useToast } from './components/toaster.tsx'
+import { EpicToaster } from './components/ui/sonner.tsx'
 import { ThemeSwitch, useTheme } from './routes/resources+/theme-switch.tsx'
 import { useNonce } from './utils/nonce-provider.ts'
 import rootLinkElements from './utils/providers/rootLinkElements.ts'
@@ -19,21 +22,34 @@ export default function App() {
 	const data = useLoaderData<typeof loader | null>()
 	const nonce = useNonce()
 	const theme = useTheme()
+	useToast(data?.toast)
 
 	return (
-		<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
-			<div className="flex h-screen flex-col justify-between">
-				<HeaderWithSearch />
+		<AuthenticityTokenProvider token={data?.csrfToken ?? ''}>
+			<Document theme={theme} nonce={nonce} honeyProps={data?.honeyProps}>
+				<div className="flex h-screen flex-col justify-between">
+					<HeaderWithSearch />
 
-				<div className="flex-1">
-					<Outlet />
+					<div className="flex-1">
+						<Outlet />
+					</div>
+
+					<div className="container flex justify-between pb-5">
+						<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
+					</div>
+
+					<FooterMenuRight />
 				</div>
-				<FooterMenuRight />
-				<div className="container flex justify-between pb-5">
-					<ThemeSwitch userPreference={data?.requestInfo.userPrefs.theme} />
-				</div>
-			</div>
-		</Document>
+				<EpicToaster
+					closeButton
+					position="bottom-right"
+					theme={theme}
+					expand
+					richColors
+					duration={5000}
+				/>
+			</Document>
+		</AuthenticityTokenProvider>
 	)
 }
 
