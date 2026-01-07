f897235f988b0ace42bf09cfbfd977b205e7623d
TTaylorOlsenn <0325055@hc.ac.je>
2026-01-07 21:20:04 +0000

    Images added to articles
    Images added to articles. Currently, the actual image related to the article is not fetched and only the site logo is shown

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -1,13 +1,27 @@
+import { getArticleImgSrc } from '~/utils/misc.tsx'
+import logo from '~/assets/png/epic-news-logo.png'
+
 interface ArticleCardProps {
 	title: string
 	category?: string
+	objectKey?: string
 }
 
-export default function ArticleCard({ title, category }: ArticleCardProps) {
+export default function ArticleCard({
+	title,
+	category,
+	objectKey,
+}: ArticleCardProps) {
+	const imageSrc = objectKey ? getArticleImgSrc(objectKey) : logo
 	return (
-		<div className="line-clamp-3 flex h-60 flex-col justify-between gap-4 rounded-xl bg-red-900 p-4 hover:bg-red-800">
-			<h3>{title}</h3>
-			<p>{category || 'General News'}</p>
+		<div>
+			<div>
+				<img src={imageSrc} alt={title} />
+			</div>
+			<div className="line-clamp-3 flex h-60 flex-col justify-between gap-4 rounded-xl bg-red-900 p-4 hover:bg-red-800">
+				<h3>{title}</h3>
+				<p>{category || 'General News'}</p>
+			</div>
 		</div>
 	)
 }
