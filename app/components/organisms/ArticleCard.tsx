14ec33ae3388ef4f0450ac70cd3fdc9c80ef73f0
TTaylorOlsenn <0325055@hc.ac.je>
2026-01-15 05:27:20 +0000

    Article pages accessible from news index

--- app/components/organisms/ArticleCard.tsx
+++ app/components/organisms/ArticleCard.tsx
@@ -1,31 +1,36 @@
+import { Link } from 'react-router'
 import siteLogo from '#app/assets/png/epic-news-logo.png'
 import { getArticleImgSrc } from '~/utils/misc.tsx'
 
 interface ArticleCardProps {
+	articleId: string
 	title: string
 	category?: string
 	objectKey?: string
 }
 
 export default function ArticleCard({
+	articleId,
 	title,
 	category,
 	objectKey,
 }: ArticleCardProps) {
 	const imageSrc = objectKey ? getArticleImgSrc(objectKey) : siteLogo
 	return (
-		<div className="cursor-pointer transition-all duration-150 hover:scale-105">
-			<div>
-				<img
-					src={imageSrc}
-					alt={title}
-					className="h-48 w-full rounded-xl object-cover"
-				/>
+		<Link to={`/article/${articleId}`}>
+			<div className="cursor-pointer transition-all duration-150 hover:scale-105">
+				<div>
+					<img
+						src={imageSrc}
+						alt={title}
+						className="h-48 w-full rounded-xl object-cover"
+					/>
+				</div>
+				<div className="line-clamp-3 flex h-60 flex-col justify-between gap-4 rounded-xl bg-red-900 p-4 hover:bg-red-800">
+					<h3>{title}</h3>
+					<p>{category || 'General News'}</p>
+				</div>
 			</div>
-			<div className="line-clamp-3 flex h-60 flex-col justify-between gap-4 rounded-xl bg-red-900 p-4 hover:bg-red-800">
-				<h3>{title}</h3>
-				<p>{category || 'General News'}</p>
-			</div>
-		</div>
+		</Link>
 	)
 }
